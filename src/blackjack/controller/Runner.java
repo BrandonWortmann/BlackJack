09a376c3bd8348aba7e87dab88bcd6d4d955079c
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-02-07 12:14:50 -0700

    I created the frame and controller files

---
+++ src/blackjack/controller/Runner.java
@@ -0,0 +1,6 @@
+package blackjack.controller;
+
+public class Runner
+{
+
+}
---
+++ src/blackjack/view/BlackJackFrame.java
@@ -0,0 +1,12 @@
+package blackjack.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import blackjack.controller.Controller;
+
+public class BlackJackFrame extends JFrame
+{
+	
+
+}
